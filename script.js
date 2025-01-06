206128e406d9ad314dff90a9347deaaceea27349
anash453 <anash6291@gmail.com>
2025-01-06 16:46:24 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 let menuList = document.getElementById("menuList")
-        menuList.style.maxHeight = "0px";
+    menuList.style.maxHeight = "0px";
 
-        function toggleMenu(){
-            if(menuList.style.maxHeight == "0px")
-            {
-                menuList.style.maxHeight = "300px";
-            }
-            else{
-                menuList.style.maxHeight = "0px";
-            }
+    function toggleMenu(){
+        if(menuList.style.maxHeight == "0px"){
+            menuList.style.maxHeight = "300px";
         }
-        window.addEventListener('scroll', function() {
-            const navbar = document.querySelector('nav');
-            if (window.scrollY > 10) {
-                navbar.classList.add('fixed');
-            } else {
-                    navbar.classList.remove('fixed');
-            }
-            });
+        else{
+            menuList.style.maxHeight = "0px";
+        }
+}
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('nav');
+    if (window.scrollY > 10) {
+        navbar.classList.add('fixed');
+    } else {
+        navbar.classList.remove('fixed');
+    }
+});
+const headerBoxes = document.querySelectorAll('.header-box');
+
+headerBoxes.forEach((box) => {
+  box.addEventListener('mouseenter', () => {
+    box.style.backgroundPosition = 'center bottom'; 
+  });
+  box.addEventListener('mouseleave', () => {
+    box.style.backgroundPosition = 'center top';
+  });
+});
+
+            
